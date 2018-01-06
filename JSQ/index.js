c3c8e73a3ddec9c7612611e69a43cae2da6bccd0
胡豆豆 <604660039@qq.com>
2018-01-06 17:23:19 +0800

    hover效果移除

--- JSQ/index.js
+++ JSQ/index.js
@@ -27,33 +27,33 @@ $(document).ready(function(e) {
 	
 	//教师学生页 触碰图标动态
 	
-	$(".grid-demo1").mouseenter(function(){ 
-		$("i[class='layui-icon 1']").addClass('layui-anim layui-anim-scaleSpring');
-	});
-	$(".grid-demo1").mouseleave(function(){ 
-		$("i[class='layui-icon 1 layui-anim layui-anim-scaleSpring']").removeClass('layui-anim layui-anim-scaleSpring');
-	});
+	// $(".grid-demo1").mouseenter(function(){ 
+	// 	$("i[class='layui-icon 1']").addClass('layui-anim layui-anim-scaleSpring');
+	// });
+	// $(".grid-demo1").mouseleave(function(){ 
+	// 	$("i[class='layui-icon 1 layui-anim layui-anim-scaleSpring']").removeClass('layui-anim layui-anim-scaleSpring');
+	// });
 	
-	$(".grid-demo2").mouseenter(function(){ 
-		$("i[class='layui-icon 2']").addClass('layui-anim layui-anim-scaleSpring');
-	});
-	$(".grid-demo2").mouseleave(function(){ 
-		$("i[class='layui-icon 2 layui-anim layui-anim-scaleSpring']").removeClass('layui-anim layui-anim-scaleSpring');
-	});
+	// $(".grid-demo2").mouseenter(function(){ 
+	// 	$("i[class='layui-icon 2']").addClass('layui-anim layui-anim-scaleSpring');
+	// });
+	// $(".grid-demo2").mouseleave(function(){ 
+	// 	$("i[class='layui-icon 2 layui-anim layui-anim-scaleSpring']").removeClass('layui-anim layui-anim-scaleSpring');
+	// });
 	
-	$(".grid-demo3").mouseenter(function(){ 
-		$("i[class='layui-icon 3']").addClass('layui-anim layui-anim-scaleSpring');
-	});
-	$(".grid-demo3").mouseleave(function(){ 
-		$("i[class='layui-icon 3 layui-anim layui-anim-scaleSpring']").removeClass('layui-anim layui-anim-scaleSpring');
-	});
+	// $(".grid-demo3").mouseenter(function(){ 
+	// 	$("i[class='layui-icon 3']").addClass('layui-anim layui-anim-scaleSpring');
+	// });
+	// $(".grid-demo3").mouseleave(function(){ 
+	// 	$("i[class='layui-icon 3 layui-anim layui-anim-scaleSpring']").removeClass('layui-anim layui-anim-scaleSpring');
+	// });
 	
-	$(".grid-demo4").mouseenter(function(){ 
-		$("i[class='layui-icon 4']").addClass('layui-anim layui-anim-scaleSpring');
-	});
-	$(".grid-demo4").mouseleave(function(){ 
-		$("i[class='layui-icon 4 layui-anim layui-anim-scaleSpring']").removeClass('layui-anim layui-anim-scaleSpring');
-	});
+	// $(".grid-demo4").mouseenter(function(){ 
+	// 	$("i[class='layui-icon 4']").addClass('layui-anim layui-anim-scaleSpring');
+	// });
+	// $(".grid-demo4").mouseleave(function(){ 
+	// 	$("i[class='layui-icon 4 layui-anim layui-anim-scaleSpring']").removeClass('layui-anim layui-anim-scaleSpring');
+	// });
 	
 
 });
