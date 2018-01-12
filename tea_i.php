693189719d91b7d26b9f055b8341a82de14bcd2b
胡豆豆 <604660039@qq.com>
2018-01-12 15:58:10 +0800

    20180112

--- tea_i.php
+++ tea_i.php
@@ -96,7 +96,7 @@ $(document).ready(function(e) {
       <li class="layui-nav-item">
         <!--修改密码-->
         <?php
-        $sql="select tid from sch_stub where tpass=''";
+        $sql="select tid from sch_stub where tpass is null";
         $rs=mysql_query($sql,$con);
         if($row=mysql_fetch_row($rs))
         {
@@ -140,7 +140,7 @@ $(document).ready(function(e) {
           <dd>
           <!--修改密码-->
           <?php
-            $sql="select tid from sch_stub where tpass=''";
+            $sql="select tid from sch_stub where tpass is null";
             $rs=mysql_query($sql,$con);
             if($row=mysql_fetch_row($rs))
             {
