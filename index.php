21eab065731289272c219f68a509eb7e6c423991
胡豆豆 <604660039@qq.com>
2018-04-06 17:49:51 +0800

    20180405

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
-	<meta name="renderer" content="webkit">
+	<meta name="renderer" content="webkit"/>
+	<meta name="force-rendering" content="webkit"/>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
 	<link rel="shortcut icon" href="favicon.ico" />
 	<meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1">
 	<link rel="stylesheet" href="layui/css/layui.css">
