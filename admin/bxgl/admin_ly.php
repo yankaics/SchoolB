16c9b65895f2b3d76a29a0c4e3e27c7fb5df93fd
胡豆豆 <604660039@qq.com>
2018-06-02 17:09:52 +0800

    全面修改后台报修分配

--- admin/bxgl/admin_ly.php
+++ admin/bxgl/admin_ly.php
@@ -12,23 +12,26 @@
  * @license   https://opensource.org/licenses/Apache-2.0 (Apache License, Version 2.0)
  */
 -->
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
-	<script src="../../JSQ/jquery-2.1.1.min.js"></script>
-	<!-- 最新版本的 Bootstrap 核心 CSS 文件 -->
-	<link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	<!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
-	<script src="../../bootstrap/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-	<!---->
-	<meta name="viewport" content="width=device-width,initial-scale=1.0" />
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1">
+	<link rel="stylesheet" href="../../layui/css/layui.css">
+	<script src="../../layui/layui.js"></script>
 	<link rel="shortcut icon" href="../../favicon.ico" />
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<!--JSQ-->
+	<script src="../../JSQ/jquery-2.1.1.min.js"></script>
+	<script src="../../JSQ/index.js"></script>
+	<!-- 最新版本的 Bootstrap 核心 CSS 文件 -->
+	<link href="../../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
+	<script src="../../bootstrap/js/bootstrap.min.js"></script>
 	<title>报修分配</title>
-	<link media="(max-width:650px)" href="../../CSS/mobile-ly-admin-index.css" rel="stylesheet" type="text/css" />
+	<!-- <link media="(max-width:650px)" href="../../CSS/mobile-ly-admin-index.css" rel="stylesheet" type="text/css" />
 	<link media="(max-width:500px)" href="../../CSS/mobile-top.css" rel="stylesheet" type="text/css" />
 	<link media="(min-width:500px)" href="../../CSS/ly-admin-index.css" rel="stylesheet" type="text/css"/>
-	<link media="(min-width:500px)" href="../../CSS/top-index.css" rel="stylesheet" type="text/css" />
+	<link media="(min-width:500px)" href="../../CSS/top-index.css" rel="stylesheet" type="text/css" /> -->
 	<style type="text/css">
 	a:link {
 		text-decoration: none;
@@ -42,55 +45,55 @@
 	a:active {
 		text-decoration: none;
 	}
+	body{ 
+		
+		margin:10px;
+	}
 	</style>
 	<script type="text/javascript">
-	function Trim(strValue) 
-	{ 
-	//return strValue.replace(/^s*|s*$/g,""); 
-	return strValue;  
-	}
+		function Trim(strValue) 
+		{ 
+			//return strValue.replace(/^s*|s*$/g,""); 
+			return strValue;  
+		}
 
-	function SetCookie(sName,sValue) 
-	{ 
-	document.cookie = sName + "=" + escape(sValue); 
-	} 
+		function SetCookie(sName,sValue) 
+		{ 
+			document.cookie = sName + "=" + escape(sValue); 
+		} 
 
-	function GetCookie(sName) 
-	{ 
-	var aCookie = document.cookie.split(";"); 
-	for(var　i=0;　i　< aCookie.length;　i++) 
-	{ 
-	var aCrumb = aCookie[i].split("="); 
-	if(sName　== Trim(aCrumb[0])) 
-	{ 
-	return unescape(aCrumb[1]); 
-	} 
-	} 
+		function GetCookie(sName) 
+		{ 
+			var aCookie = document.cookie.split(";"); 
+			for(var　i=0;　i　< aCookie.length;　i++) 
+			{ 
+				var aCrumb = aCookie[i].split("="); 
+				if(sName　== Trim(aCrumb[0])) 
+				{ 
+					return unescape(aCrumb[1]); 
+				} 
+			} 
 
-	　　return null; 
-	} 
+		　　return null; 
+		} 
 
-	function scrollback() 
-	{ 
-	if(GetCookie("scroll")!=null){document.body.scrollTop=GetCookie("scroll")} 
-	} 
+		function scrollback() 
+		{ 
+			if(GetCookie("scroll")!=null){document.body.scrollTop=GetCookie("scroll")} 
+		} 
 
-	function checkfp()
-	{
-		if(!$(".choo").is(":checked"))
+		function checkfp()
 		{
-			alert("请勾选需要分配的任务");
-			return false;
-		}		
-	}
-	</script>
-
-	<script language="javascript">
-	setTimeout("self.location.reload();",60*10000);
+			if(!$(".choo").is(":checked"))
+			{
+				alert("请勾选需要分配的任务");
+				return false;
+			}		
+		}
 	</script>
 </head>
 
-<body id=body onscroll=SetCookie("scroll",document.body.scrollTop); onload="scrollback();">
+<body>
 <!--导航
 <div class="top-index">
 	<div class="logo"><img src="../../UI/logo/logogif.gif"></div>
@@ -100,232 +103,229 @@
   </div>
 </div>-->
 <!--main-->
-<center>
-<?
-include("../../PHP/riqi.php");
-include("../../SQL/db/db.php");
-include("../../PHP/adminse.php");
-include("../adminse/admin_se.php");
-include("address.php");//地点
+<?php
+	include("../../PHP/riqi.php");
+	include("../../SQL/db/db.php");
+	include("../../PHP/adminse.php");
+	include("../adminse/admin_se.php");
+	include("address.php");//地点
 
-$sqlall="select count(sid) from sch_repair_re where s_jg='未处理' and s_repair='未分配'";
-$rsall=mysql_query($sqlall,$con);
-if($rowall=mysql_fetch_row($rsall))
-	$numall=$rowall[0];
-
-for($i=0;$i<count($arrayall);$i++)
-{
-	$sql="select count(sid) from sch_repair_re where s_add='".$arrayall[$i]."' and s_jg='未处理' and s_repair='未分配'";
-	$rs=mysql_query($sql,$con);
-	if($row=mysql_fetch_row($rs))
-		$arraycount[$i]=$row[0];//计数
-}
-
-?>
-<div class="ly">
-	<h2>报修分配</h2>
-    <p>
-    <form class="form-horizontal" action="" method="get" role="form">
-      <p><button type="submit" name="all" class="btn btn-default">所有<span class="badge"><?=$numall?></span></button>
-      	<?php
-      		for($i=0;$i<count($arrayall);$i++)
-			{
-      	?>
-          <button type="submit" name="<?=$arrayalldm[$i]?>" class="btn btn-default"><?=$arrayall[$i]?><span class="badge"><?=$arraycount[$i]?></span></button>
-        <?
-        	}
-        ?>
-      </p>
-    </form>
-          <form name="wxyfp" action="" method="get" onsubmit="return checkfp()">
-          <span class="input-group-addon">
-          	<p>默认显示所有未分配</p><p>选择进行分配</p>
-            <p>
-            <div class="form-group">
-                <div class="col-sm-offset-5 col-sm-2">
-                    <span>
-                    <select name="wxy" class="form-control" id="tt2">
-                
-                      <?
-                      $sqlwx="select * from sch_admin where s_position='维修员' and s_g!=0";
-                      $rswx=mysql_query($sqlwx,$con);
-                      while($rowwx=mysql_fetch_row($rswx))
-                      {
-                      ?>
-                      <option value="<?=$rowwx[3]?>"><?=$rowwx[3]?></option>
-                      <?
-                      }
-                      ?>
-                    </select>
-                    <button name="buttonfp" onclick="return confirm('确定分配？');" type="submit" class="btn btn-default">分配</button>
-            	</div>
-            </div>
-            </p>
-          </span>
-       <p>
-       
-       </p>
-    
-    </p>
-    
-    <p>
-    <div class="table-responsive">
-      <table width="90%" class="table" border="1" cellspacing="0" cellpadding="0">
-   
-  <tr>
-  	
-    <td align="center" class="text-danger"><input name="allc" id="allc" class="allc" type="checkbox" value="allc"  style=" width:20px;" ><label for="allc">全选</label> |  删除</td>
-    <td align="center" class="text-danger">地点</td>
-    <td align="center" class="text-danger">姓名</td>
-    <td align="center" class="text-danger">电话</td>
-    <td align="center" class="text-danger">专业或部门</td>
-    <td align="center" class="text-danger">报修时间 </td>
-    <td align="center" class="text-danger">损坏描述</td>
-    <td align="center" class="text-danger">物件详情</td>
-  </tr>
-  <?
-  
-	if(isset($_GET['all']))
-	{
-		$sqlre="select * from sch_repair_re where s_jg='未处理' and s_repair='未分配' order by s_settime asc";
-		$b='all=';
-	}
-	else
-	{
-		$sqlre="select * from sch_repair_re where s_repair='未分配' and s_jg='未处理' order by s_settime asc";
-	}
+	$sqlall="select count(sid) from sch_repair_re where s_jg='未处理' and s_repair='未分配'";
+	$rsall=mysql_query($sqlall,$con);
+	if($rowall=mysql_fetch_row($rsall))
+		$numall=$rowall[0];
 
 	for($i=0;$i<count($arrayall);$i++)
 	{
-		if(isset($_GET[$arrayalldm[$i]]))
-		{
-			$sqlre="select * from sch_repair_re where s_add='".$arrayall[$i]."' and s_jg='未处理' and s_repair='未分配' order by s_settime asc";
-			$b=$arrayalldm[$i];
-		}
+		$sql="select count(sid) from sch_repair_re where s_add='".$arrayall[$i]."' and s_jg='未处理' and s_repair='未分配'";
+		$rs=mysql_query($sql,$con);
+		if($row=mysql_fetch_row($rs))
+			$arraycount[$i]=$row[0];//计数
 	}
-   $rsre=mysql_query($sqlre,$con);
-   while($rowre=mysql_fetch_row($rsre))
-   {
-   ?>
-  <tr>
-    <td align="center">
-    	<input name="c[]" id="<?=$rowre[0]?>" class="choo" type="checkbox" value="<?=$rowre[0]?>"  style=" width:20px;" ><label for="<?=$rowre[0]?>">选择</label>
-    	<input name="tb" type="hidden" value="<?=$b?>" />
-   
-	</form><span>
-    <a href="delete_adminly.php?tname=<?=$rowre[3]?>&tphone=<?=$rowre[5]?>&tadd=<?=$rowre[1]?>&ttime=<?=$rowre[10]?>&b=<?=$b?>"><button onclick="return confirm('确定删除？');" type="button" class="btn btn-default">删除</button></a></span>
-    </td>
-    <td align="center"><?=$rowre[1].$rowre[2]?></td>
-    <td align="center"><?=$rowre[3]?></td>
-    <td align="center"><?=$rowre[5]?></td>
-    <td align="center"><?=$rowre[4]?></td>
-    <td align="center"><?=$rowre[10]?></td>
-    <td align="center"><button type="button" onclick="return confirm('<?=$rowre[14]?>');" name="shms" class="btn btn-default">损坏描述</button></td>
-    
-    <td align="center">
-    <form action="" method="get">
-    <?
-    if(isset($_GET['tb']))
-	{
-	?>
-    <input name="tb" type="hidden" value="<?=$_GET['tb']?>" />
-    <?
-	}
-	else
-	{
-	?>
-    <input name="tb" type="hidden" value="<?=$b?>" />
-    <?
-	}
-	?>
-    <input name="tname" type="hidden" value="<?=$rowre[3]?>" />
-    <input name="tphone" type="hidden" value="<?=$rowre[5]?>" />
-    <input name="tadd" type="hidden" value="<?=$rowre[1]?>" />
-    <input name="ttime" type="hidden" value="<?=$rowre[10]?>" />
-    <button type="submit" name="wjxq" class="btn btn-default">物件详情</button>
-    </form>
-    </td>
-    <?
-	}
-	?>
-    
-  </tr>
-</table>
-<!--物件详情-->
-<?
-if(isset($_GET['wjxq']))
-{
-?>
-<script language="javascript">
-	alert('<? 
-    $sqlrea="select * from sch_repair_rea where s_time='".$_GET['ttime']."' and s_name='".$_GET['tname']."' and s_phone='".$_GET['tphone']."' and s_add='".$_GET['tadd']."'";
-	$rsrea=mysql_query($sqlrea,$con);
-	while($rowrea=mysql_fetch_row($rsrea))
-	{
-		echo "（物件:".$rowrea[1];
-		echo "-数量:".$rowrea[2]."）\\n";
-	}
-	?>');
-	location.href="admin_ly.php?<?=$_GET['tb']?>";
-</script>
-<?
-}
-?>
-<script type="text/javascript">
-$("#allc").change(function(){
-	var innum=$(".choo").length;
-	if($(this).prop("checked")){
-		$(".choo").prop("checked",true);
-		$("#in_num").text(innum);
-		$(".lskdo").val(1);
-		$("#sp_num").text(innum);
-		$(".lskdo").prop("disabled",false);
-	}
-	else{
-		$(".choo").prop("checked",false);
-		$("#in_num").text(0);
-		$(".lskdo").val(0);
-		$("#sp_num").text(0);
-		$(".lskdo").prop("disabled",true);
-		}
-	})
-	
-	</script>
-<!--分配-->
-<?
-if(isset($_GET['buttonfp']))
-{	
-	$nc=$_GET['c'];
-	$n=count($_GET['c']);
-	$tb=$_GET['tb'];
-	$wxy=$_GET['wxy'];
-	for($i=0;$i<=$n-1;$i++)
-	{
-		$sql_inewxy="select * from sch_repair_re where sid='".$nc[$i]."'";
-		$rs_inewxy=mysql_query($sql_inewxy,$con);
-		if($rowewxy=mysql_fetch_row($rs_inewxy))
-		{
-			$sql_update="update sch_repair_rea set s_repair='".$wxy."' where s_time='".$rowewxy[10]."' and s_add='".$rowewxy[1]."' and s_name='".$rowewxy[3]."' and s_phone='".$rowewxy[5]."'";
-			$rs_update=mysql_query($sql_update,$con);
-		}
-		$sql_inwxy="update sch_repair_re set s_repair='".$wxy."' where sid='".$nc[$i]."'";
-		$rs_inwxy=mysql_query($sql_inwxy,$con);
-	}
-	if($rs_inwxy>0)
-	{
-		/*
-		短信平台：http://www.sms.cn/
-		 
-		$requesturl='http://api.sms.cn/sms/?ac=send&uid=amos&pwd=7191d511822634f5783dc89baeea616d&template=418231&mobile=15111888341&content={"name":"'.$wxy.'"}';
-	    //curl方式获取json数组
-	    $curl = curl_init(); //初始化
-	    curl_setopt($curl, CURLOPT_URL, $requesturl);//设置抓取的url
-	    curl_setopt($curl, CURLOPT_HEADER, 0);//设置头文件的信息作为数据流输出
-	    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);//设置获取的信息以文件流的形式返回，而不是直接输出。
-	    $data = curl_exec($curl);//执行命令
-	    curl_close($curl);//关闭URL请求
 
-	    */
+?>
+<div class="ly">
+	<blockquote class="layui-elem-quote">
+		<h3>报修分配</h3>
+		<p>对未分配报修进行分配</p>
+		<!--地点选择-->
+	    <form class="" action="" method="get" role="form">
+	    	<div class="table-responsive">
+	    		<table width="100%" class="table" border="0" cellspacing="0" cellpadding="0">
+	    			<tr>
+	    				<td>
+				      		<button type="submit" name="all" class="layui-btn layui-btn-sm layui-btn-normal">所有<span class="layui-badge"><?=$numall?></span></button>
+				      	<?php
+				      		for($i=0;$i<count($arrayall);$i++)
+							{
+				      	?>
+				          	<button type="submit" name="<?=$arrayalldm[$i]?>" class="layui-btn layui-btn-sm"><?=$arrayall[$i]?><span class="layui-badge"><?=$arraycount[$i]?></span></button>
+				        <?php
+				        	}
+				        ?>
+				    	</td>
+			    	</tr>
+	    		</table>
+	      	</div>
+	    </form>
+	</blockquote>
+	<!--维修员分配-->
+	<form name="wxyfp" action="" method="get" onsubmit="return checkfp()">
+		<blockquote class="layui-elem-quote">
+			<div class="layui-form layui-form-pane">
+
+		    	<div class="layui-form-item">
+				    <label class="layui-form-label" style="width:100px;">维修员</label>
+				    <div class="layui-input-inline">
+				      	<select name="wxy" class="" id="tt2">
+							<?php
+								$sqlwx="select * from sch_admin where s_position='维修员' and s_g!=0";
+								$rswx=mysql_query($sqlwx,$con);
+								while($rowwx=mysql_fetch_row($rswx))
+								{
+							?>
+								<option value="<?=$rowwx[3]?>"><?=$rowwx[3]?></option>
+							<?php
+								}
+							?>
+				        </select>
+				    </div>
+				    <button name="buttonfp" onclick="return confirm('确定分配？');" type="submit" class="layui-btn layui-form-mid" style="width: 80px;">分配</button>
+				</div>
+			</div>
+		</blockquote>
+	
+
+
+
+	    <div class="table-responsive">
+	      	<table width="100%" class="layui-table table" lay-even>
+	      		<colgroup>
+					<col width="100">
+					<col width="100">
+					<col width="100">
+					<col width="300">
+					<col width="80">
+					<col width="100">
+					<col width="200">
+					<col width="160">
+					
+				</colgroup>
+	      		<thead>
+			  		<tr>
+					    <td align="center" class="">
+					    	<input name="allc" id="allc" class="allc" type="checkbox" value="allc"  style=" width:20px;" ><label for="allc">全选</label>&nbsp;·&nbsp;删除
+					    </td>
+					    <td align="center" class="">损坏描述</td>
+					    <td align="center" class="">物件详情</td>
+					    <td align="center" class="">地点</td>
+					    <td align="center" class="">姓名</td>
+					    <td align="center" class="">电话</td>
+					    <td align="center" class="">专业或部门</td>
+					    <td align="center" class="">报修时间 </td>
+					    
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+						if(isset($_GET['all']))
+						{
+							$sqlre="select * from sch_repair_re where s_jg='未处理' and s_repair='未分配' order by s_settime asc";
+							$b='all=';
+						}
+						else
+						{
+							$sqlre="select * from sch_repair_re where s_repair='未分配' and s_jg='未处理' order by s_settime asc";
+						}
+
+						for($i=0;$i<count($arrayall);$i++)
+						{
+							if(isset($_GET[$arrayalldm[$i]]))
+							{
+								$sqlre="select * from sch_repair_re where s_add='".$arrayall[$i]."' and s_jg='未处理' and s_repair='未分配' order by s_settime asc";
+								$b=$arrayalldm[$i];
+							}
+						}
+						$rsre=mysql_query($sqlre,$con);
+						while($rowre=mysql_fetch_row($rsre))
+						{
+					?>
+				  	<tr>
+					    <td align="center">
+					    	<input name="c[]" id="<?=$rowre[0]?>" class="choo" type="checkbox" value="<?=$rowre[0]?>"  style=" width:20px;" ><label for="<?=$rowre[0]?>">选择</label>
+					    	<input name="tb" type="hidden" value="<?=$b?>" />
+					   
+							
+
+							<span>&nbsp;<a href="delete_adminly.php?tname=<?=$rowre[3]?>&tphone=<?=$rowre[5]?>&tadd=<?=$rowre[1]?>&ttime=<?=$rowre[10]?>&b=<?=$b?>"><button onclick="return confirm('确定删除？');" type="button" class="layui-btn layui-btn-xs layui-btn-danger">删除</button></a></span>
+						</td>
+						<td align="center"><button type="button" onclick="return confirm('<?=$rowre[14]?>');" name="shms" class="layui-btn">损坏描述</button></td>
+					    
+					    <td align="center">
+							    <button type="button" onclick="return confirm('<? 
+			    $sqlrea="select * from sch_repair_rea where s_time='".$rowre[10]."' and s_name='".$rowre[3]."' and s_phone='".$rowre[5]."' and s_add='".$rowre[1]."'";
+				$rsrea=mysql_query($sqlrea,$con);
+				while($rowrea=mysql_fetch_row($rsrea))
+				{
+					echo "（物件:".$rowrea[1];
+					echo "-数量:".$rowrea[2]."）\\n";
+				}
+				?>');" name="wjxq" class="layui-btn">物件详情</button>
+					    </td>
+					    <td align="center"><?=$rowre[1].$rowre[2]?></td>
+					    <td align="center"><?=$rowre[3]?></td>
+					    <td align="center"><?=$rowre[5]?></td>
+					    <td align="center"><?=$rowre[4]?></td>
+					    <td align="center"><?=$rowre[10]?></td>
+					    
+				    <?php
+						}
+					?>
+					    
+				  	</tr>
+				</tbody>
+			</table>
+		</div>
+	</form><!--form name="wxyfp"-->
+
+</div>
+
+	<script type="text/javascript">
+		//多选
+		$("#allc").change(function(){
+			var innum=$(".choo").length;
+			if($(this).prop("checked")){
+				$(".choo").prop("checked",true);
+				$("#in_num").text(innum);
+				$(".lskdo").val(1);
+				$("#sp_num").text(innum);
+				$(".lskdo").prop("disabled",false);
+			}
+			else{
+				$(".choo").prop("checked",false);
+				$("#in_num").text(0);
+				$(".lskdo").val(0);
+				$("#sp_num").text(0);
+				$(".lskdo").prop("disabled",true);
+				}
+			})
+		
+	</script>
+	<!--分配-->
+	<?php
+		if(isset($_GET['buttonfp']))
+		{	
+			$nc=$_GET['c'];
+			$n=count($_GET['c']);
+			$tb=$_GET['tb'];
+			$wxy=$_GET['wxy'];
+			for($i=0;$i<=$n-1;$i++)
+			{
+				$sql_inewxy="select * from sch_repair_re where sid='".$nc[$i]."'";
+				$rs_inewxy=mysql_query($sql_inewxy,$con);
+				if($rowewxy=mysql_fetch_row($rs_inewxy))
+				{
+					$sql_update="update sch_repair_rea set s_repair='".$wxy."' where s_time='".$rowewxy[10]."' and s_add='".$rowewxy[1]."' and s_name='".$rowewxy[3]."' and s_phone='".$rowewxy[5]."'";
+					$rs_update=mysql_query($sql_update,$con);
+				}
+				$sql_inwxy="update sch_repair_re set s_repair='".$wxy."' where sid='".$nc[$i]."'";
+				$rs_inwxy=mysql_query($sql_inwxy,$con);
+			}
+			if($rs_inwxy>0)
+			{
+				/*
+				短信平台：http://www.sms.cn/
+				 
+				$requesturl='http://api.sms.cn/sms/?ac=send&uid=amos&pwd=7191d511822634f5783dc89baeea616d&template=418231&mobile=15111888341&content={"name":"'.$wxy.'"}';
+			    //curl方式获取json数组
+			    $curl = curl_init(); //初始化
+			    curl_setopt($curl, CURLOPT_URL, $requesturl);//设置抓取的url
+			    curl_setopt($curl, CURLOPT_HEADER, 0);//设置头文件的信息作为数据流输出
+			    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);//设置获取的信息以文件流的形式返回，而不是直接输出。
+			    $data = curl_exec($curl);//执行命令
+			    curl_close($curl);//关闭URL请求
+
+			    */
 				?>
 		        <script language="javascript">
 		        	alert("分配成功：<?=$wxy?>");
@@ -333,12 +333,15 @@ if(isset($_GET['buttonfp']))
 		        </script>
 		        <?
 			}
-			
+					
 		}
-?>
-</div>
-    </p>
-</div>
-</center>
+	?>
+
+	<script>
+	//表单
+	layui.use('form', function(){
+	  var form = layui.form;
+	});
+	</script>
 </body>
 </html>
