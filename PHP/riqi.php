992a2cd1698652a199087527d517638426816925
AmosHuKe <amoshuke@qq.com>
2018-06-09 17:05:17 +0800

    日期修改

--- PHP/riqi.php
+++ PHP/riqi.php
@@ -15,9 +15,9 @@
 $rqrq=date_default_timezone_set("Asia/shanghai");
 $rqm=date("n"); //不带0的月
 $rqmm=date("m"); //带0的月
-$rqY=date("Y"); //年
-$rqd=date("d"); 
-$rqH=date("H"); 
-$rqi=date("i");
-$rqs=date("s");
+$rqY=date("Y"); //完整年
+$rqd=date("d"); //带0的日
+$rqH=date("H"); //24 小时格式，有前导零
+$rqi=date("i");//带0的分
+$rqs=date("s");//带0的秒
 ?>
