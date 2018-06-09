b7449f9b230653c8ea1c302cfdcac4fb7bfb5bd6
AmosHuKe <amoshuke@qq.com>
2018-06-09 17:04:52 +0800

    宿管管理详情修改

--- admin/zhgl/zhglsg_re.php
+++ admin/zhgl/zhglsg_re.php
@@ -73,6 +73,14 @@
         	 <br>
             <p>
             <form class="layui-form" method="get" action="">
+              <input name="nid" type="hidden" value="<?=$_GET['nid']?>" />
+                <div class="layui-input-inline">
+                  <input type="text" name="tlsh" required  lay-verify="required" placeholder="请输入流水号" autocomplete="off" class="layui-input">
+                </div>
+                <button class="layui-btn" name="lsh" lay-submit type="submit">查询流水号</button>
+              
+            </form>
+            <form class="layui-form" method="get" action="">
                 <input name="nid" type="hidden" value="<?=$_GET['nid']?>" />
                   <div class="layui-row">
                     <div class="layui-col-md6">
@@ -122,34 +130,42 @@
       </colgroup>
       <thead>
         <tr>
+          <th lay-data="{field:'serial',width:300}" align="left">流水号</th>
           <th lay-data="{field:'dor',width:200}" align="left">寝室号</th>
           <th lay-data="{field:'yjf',width:100}" align="left">已缴费</th>
           <th lay-data="{field:'loadt',width:150}" align="left">电费上传时间</th>
           <th lay-data="{field:'jft',width:150}" align="left">缴费时间</th>
+          <th lay-data="{field:'accounts',width:300}" align="left">是否扎帐</th>
         </tr> 
       </thead>
       <tbody>
       	<?
       	if(isset($_GET['button']))
-		{
-			$sql="select * from sch_dfre where s_time='".$_GET['da1'].'-'.$_GET['da2']."' and s_username='".$_GET['nid']."'";
-      ?>
-      <script type="text/javascript">
-          //时间
-          document.getElementById("sadminY").value = "<?=$_GET['da1']?>";
-          document.getElementById("sadminm").value = "<?=$_GET['da2']?>";
-        </script>
-      <?
-		}
+    		{
+    			$sql="select * from sch_dfre where s_time='".$_GET['da1'].'-'.$_GET['da2']."' and s_username='".$_GET['nid']."'";
+          ?>
+          <script type="text/javascript">
+              //时间
+              document.getElementById("sadminY").value = "<?=$_GET['da1']?>";
+              document.getElementById("sadminm").value = "<?=$_GET['da2']?>";
+            </script>
+          <?
+    		}
+        else if(isset($_GET['lsh']))
+        {
+          $sql="select * from sch_dfre where s_username='".$_GET['nid']."' and s_serial='".$_GET['tlsh']."'";
+        }
 		$rs=mysql_query($sql,$con);
 		while($rowre=mysql_fetch_row($rs))
 		{
 		?>
         <tr>
+          <td align="left"><?=$rowre[8]?></td>
           <td align="left"><?=$rowre[4]?></td>
           <td align="left"><?=$rowre[5]?></td>
           <td align="left"><?=$rowre[6]?></td>
           <td align="left"><?=$rowre[7]?></td>
+          <td align="left"><?=$rowre[9]?></td>
           
         </tr>
     <?
