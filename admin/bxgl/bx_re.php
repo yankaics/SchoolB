108b61bb92f7bab7f47937ee244778e1dfa7dc61
AmosHuKe <amoshuke@qq.com>
2018-06-10 12:38:55 +0800

    后台报修前后统计样式修改

--- admin/bxgl/bx_re.php
+++ admin/bxgl/bx_re.php
@@ -15,21 +15,23 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<script src="../../JSQ/jquery-2.1.1.min.js"></script>
-	<!-- 最新版本的 Bootstrap 核心 CSS 文件 -->
-	<link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	<!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
-	<script src="../../bootstrap/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-	<!---->
-	<script src="../../JSQ/index.js"></script>
-	<meta name="viewport" content="width=device-width,initial-scale=1.0" />
-	<link rel="shortcut icon" href="../../favicon.ico" />
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>维修前统计</title>
-	<link media="(max-width:650px)" href="../../CSS/mobile-ly-admin-index.css" rel="stylesheet" type="text/css" />
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1">
+    <link rel="stylesheet" href="../../layui/css/layui.css">
+    <script src="../../layui/layui.js"></script>
+    <link rel="shortcut icon" href="../../favicon.ico" />
+    <!--JSQ-->
+    <script src="../../JSQ/jquery-2.1.1.min.js"></script>
+    <script src="../../JSQ/index.js"></script>
+    <!-- 最新版本的 Bootstrap 核心 CSS 文件 -->
+    <link href="../../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
+    <script src="../../bootstrap/js/bootstrap.min.js"></script>
+    <title>维修前统计</title>
+	<!-- <link media="(max-width:650px)" href="../../CSS/mobile-ly-admin-index.css" rel="stylesheet" type="text/css" />
 	<link media="(max-width:500px)" href="../../CSS/mobile-top.css" rel="stylesheet" type="text/css" />
 	<link media="(min-width:500px)" href="../../CSS/ly-admin-index.css" rel="stylesheet" type="text/css"/>
-	<link media="(min-width:500px)" href="../../CSS/top-index.css" rel="stylesheet" type="text/css" />
+	<link media="(min-width:500px)" href="../../CSS/top-index.css" rel="stylesheet" type="text/css" /> -->
 	<style type="text/css">
 	a:link {
 		text-decoration: none;
@@ -43,18 +45,22 @@
 	a:active {
 		text-decoration: none;
 	}
+    body{ 
+        
+        margin:10px;
+    }
 	</style>
 	<script type="text/javascript">
-	function printHtml(html) {
-	var bodyHtml = document.body.innerHTML;
-	document.body.innerHTML = html;
-	window.print();
-	document.body.innerHTML = bodyHtml;
-	}
-	function onprint() {
-	var html = $("#dy").html();
-	printHtml(html);
-	}
+    	function printHtml(html) {
+        	var bodyHtml = document.body.innerHTML;
+        	document.body.innerHTML = html;
+        	window.print();
+        	document.body.innerHTML = bodyHtml;
+        }
+        function onprint() {
+        	var html = $("#dy").html();
+        	printHtml(html);
+    	}
 	</script>
 </head>
 
@@ -68,71 +74,95 @@
   </div>
 </div>-->
 <!--main-->
-<center>
 <?
-include("../../PHP/riqi.php");
-include("../../SQL/db/db.php");
-include("../../PHP/adminse.php");
-include("../adminse/admin_se.php");
-include("address.php");//地点
+    include("../../PHP/riqi.php");
+    include("../../SQL/db/db.php");
+    include("../../PHP/adminse.php");
+    include("../adminse/admin_se.php");
+    include("address.php");//地点
 ?>
 <div class="ly">
-  <h2>维修前物件</h2> <span class="input-group-addon"><p>选择日期，再选择地点进行统计</p><p>时间是用户报修的时间</p><input type="button" class="btn btn-default" id="btnPrint" 
-onclick="onprint()" value="打印本页" />
-    <form action="" method="get" name="bxre">
-
-    </form>
-
-    </span>
-    <p>
-    <form action="" method="get" name="bxre">
-    	<p>
-        	<?
-            if(isset($_GET['da1']))
-		  	{
-				$da1=$_GET['da1'];
-			}
-			else
-			{
-                $sqla="select s_settime from sch_repair_re where s_jg='未处理' and s_repair!='未分配' order by s_settime asc";
-                $rsa=mysql_query($sqla,$con);
-                if($rowa=mysql_fetch_row($rsa))
+    <blockquote class="layui-elem-quote">
+        <h3>维修前物件统计 <input type="button" class="layui-btn" id="btnPrint" 
+onclick="onprint()" value="打印本页" /></h3>
+        <p>选择日期，再选择地点进行统计</p>
+        <p>时间是用户报修的时间</p>
+        <form action="" class="layui-form layui-form-pane" method="get" name="bxre">
+            <?
+                if(isset($_GET['da1']))
                 {
-				    $da1=substr($rowa[0],0,10);
+                    $da1=$_GET['da1'];
                 }
                 else
                 {
-                    $da1=$rqY.'-'.$rqmm.'-'.$rqd;
+                    $sqla="select s_settime from sch_repair_re where s_jg='未处理' and s_repair!='未分配' order by s_settime asc";
+                    $rsa=mysql_query($sqla,$con);
+                    if($rowa=mysql_fetch_row($rsa))
+                    {
+                        $da1=substr($rowa[0],0,10);
+                    }
+                    else
+                    {
+                        $da1=$rqY.'-'.$rqmm.'-'.$rqd;
+                    }
                 }
-			}
-			if(isset($_GET['da2']))
-		  	{
-				$da2=$_GET['da2'];
-			}
-			else
-			{
-				$da2=$rqY.'-'.$rqmm.'-'.$rqd;
-			}
+                if(isset($_GET['da2']))
+                {
+                    $da2=$_GET['da2'];
+                }
+                else
+                {
+                    $da2=$rqY.'-'.$rqmm.'-'.$rqd;
+                }
             ?>
-    	  <input name="da1" type="date" value="<?=$da1?>"/>
-    	  至<input name="da2" type="date" value="<?=$da2?>"/>
-   	  <p>
-      	<button type="submit" name="all" class="btn btn-default">所有详情</button>
-        <button type="submit" name="alltj" class="btn btn-default">所有统计</button>
-        <p>
-        <?php
-            for($i=0;$i<count($arrayall);$i++)
-            {
-        ?>
-   	        <button type="submit" name="<?=$arrayalldm[$i]?>" class="btn btn-default"><?=$arrayall[$i]?></button>
-        <?php
-            }
-        ?>
-        </p>
-	  </p>
-        
-    </form>
-    </p>
+            <div class="layui-form-item">
+                
+                <div class="layui-inline">
+                    <label class="layui-form-label">时间</label>
+                    <div class="layui-input-inline" style="width: 140px;">
+                        <input name="da1" class="layui-input" id="da1" type="text" value="<?=$da1?>"/>
+                    </div>
+                <div class="layui-form-mid">-</div>
+                <div class="layui-input-inline" style="width: 140px;">
+                    <input name="da2" class="layui-input" id="da2" type="text" value="<?=$da2?>"/>
+                </div>
+              </div>
+            </div> 
+
+            <div class="table-responsive">
+                <table width="100%" class="table" border="0" cellspacing="0" cellpadding="0">
+                    <tr>
+                        <td>
+                            <button type="submit" name="all" class="layui-btn layui-btn-sm layui-btn-normal">所有详情</button>
+                            <button type="submit" name="alltj" class="layui-btn layui-btn-sm layui-btn-normal">所有统计</button>
+                            <?php
+                                for($i=0;$i<count($arrayall);$i++)
+                                {
+                            ?>
+                                <button type="submit" name="<?=$arrayalldm[$i]?>" class="layui-btn layui-btn-sm"><?=$arrayall[$i]?></button>
+                            <?php
+                                }
+                            ?>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <script>
+            layui.use('laydate', function(){
+              var laydate = layui.laydate;
+              
+              //执行一个laydate实例
+              laydate.render({
+                elem: '#da1' //指定元素
+              });
+              laydate.render({
+                elem: '#da2' //指定元素
+              });
+            });
+            </script>
+        </form>
+    </blockquote>
+
     <p>
     
     <div class="container">
@@ -185,7 +215,7 @@ onclick="onprint()" value="打印本页" />
 
         			?>
                         <h2><?php echo $arrayall[$i]."：".$rowa[2]?></h2>
-                        <table border="1" class="table" cellspacing="0" cellpadding="10">
+                        <table border="1" class="table " cellspacing="0" cellpadding="10">
                         
                         <tr class="top">
                             <td align="center">地点</td>
@@ -230,7 +260,7 @@ onclick="onprint()" value="打印本页" />
                             {
                     ?>
                         <h2><?php echo '<'.$arrayall[$i].'总计>'?></h2>
-                        <table border="1" class="table" cellspacing="0" cellpadding="10">
+                        <table border="1" class="table " cellspacing="0" cellpadding="10">
                         
                         <tr class="top">
                             <td align="center">物件</td>
@@ -250,7 +280,7 @@ onclick="onprint()" value="打印本页" />
                             }
                         ?>
                         </table>
-                        <hr style="background-color:#000;height: 1px;border:none;"/>
+                        <br/><br/><br/><br/>
                     <?
                            }
     				?>
@@ -269,7 +299,7 @@ onclick="onprint()" value="打印本页" />
                             {
                     ?>
                         <h2><?php echo '<'.$arrayall[$i].'总计>'?></h2>
-                        <table border="1" class="table" cellspacing="0" cellpadding="10">
+                        <table border="1" class="table " cellspacing="0" cellpadding="10">
                         
                         <tr class="top">
                             <td align="center">物件</td>
@@ -308,7 +338,7 @@ onclick="onprint()" value="打印本页" />
 			}
 			?>
             </h2>
-            <table border="1" class="table" cellspacing="0" cellpadding="10">
+            <table border="1" class="table " cellspacing="0" cellpadding="10">
                 <tr class="top">
                     <td align="center">物件</td>
                     <td align="center">数量</td>
@@ -355,6 +385,6 @@ onclick="onprint()" value="打印本页" />
 </div>
     </p>
 </div>
-</center>
+
 </body>
 </html>
--- admin/bxgl/bx_re_af.php
+++ admin/bxgl/bx_re_af.php
@@ -15,21 +15,23 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <script src="../../JSQ/jquery-2.1.1.min.js"></script>
-    <!-- 最新版本的 Bootstrap 核心 CSS 文件 -->
-    <link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
-    <script src="../../bootstrap/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    <!---->
-    <script src="../../JSQ/index.js"></script>
-    <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1">
+    <link rel="stylesheet" href="../../layui/css/layui.css">
+    <script src="../../layui/layui.js"></script>
     <link rel="shortcut icon" href="../../favicon.ico" />
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>维修前统计</title>
-    <link media="(max-width:650px)" href="../../CSS/mobile-ly-admin-index.css" rel="stylesheet" type="text/css" />
+    <!--JSQ-->
+    <script src="../../JSQ/jquery-2.1.1.min.js"></script>
+    <script src="../../JSQ/index.js"></script>
+    <!-- 最新版本的 Bootstrap 核心 CSS 文件 -->
+    <link href="../../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
+    <script src="../../bootstrap/js/bootstrap.min.js"></script>
+    <title>维修后统计</title>
+    <!-- <link media="(max-width:650px)" href="../../CSS/mobile-ly-admin-index.css" rel="stylesheet" type="text/css" />
     <link media="(max-width:500px)" href="../../CSS/mobile-top.css" rel="stylesheet" type="text/css" />
     <link media="(min-width:500px)" href="../../CSS/ly-admin-index.css" rel="stylesheet" type="text/css"/>
-    <link media="(min-width:500px)" href="../../CSS/top-index.css" rel="stylesheet" type="text/css" />
+    <link media="(min-width:500px)" href="../../CSS/top-index.css" rel="stylesheet" type="text/css" /> -->
     <style type="text/css">
     a:link {
         text-decoration: none;
@@ -43,18 +45,22 @@
     a:active {
         text-decoration: none;
     }
+    body{ 
+        
+        margin:10px;
+    }
     </style>
     <script type="text/javascript">
-    function printHtml(html) {
-    var bodyHtml = document.body.innerHTML;
-    document.body.innerHTML = html;
-    window.print();
-    document.body.innerHTML = bodyHtml;
-    }
-    function onprint() {
-    var html = $("#dy").html();
-    printHtml(html);
-    }
+        function printHtml(html) {
+            var bodyHtml = document.body.innerHTML;
+            document.body.innerHTML = html;
+            window.print();
+            document.body.innerHTML = bodyHtml;
+        }
+        function onprint() {
+            var html = $("#dy").html();
+            printHtml(html);
+        }
     </script>
 </head>
 
@@ -68,62 +74,85 @@
   </div>
 </div>-->
 <!--main-->
-<center>
 <?
-include("../../PHP/riqi.php");
-include("../../SQL/db/db.php");
-include("../../PHP/adminse.php");
-include("../adminse/admin_se.php");
-include("address.php");//地点
+    include("../../PHP/riqi.php");
+    include("../../SQL/db/db.php");
+    include("../../PHP/adminse.php");
+    include("../adminse/admin_se.php");
+    include("address.php");//地点
 ?>
 <div class="ly">
-  <h2>维修后统计</h2> <span class="input-group-addon"><p>选择日期，再选择地点进行统计</p><p>时间是维修完成的时间</p><!--<input type="button" class="btn btn-default" id="btnPrint" 
-onclick="onprint()" value="打印本页" />-->
-    <form action="" method="get" name="bxre">
-
-    </form>
-
-    </span>
-    <p>
-    <form action="" method="get" name="bxre">
-        <p>
+  <blockquote class="layui-elem-quote">
+        <h3>维修后物件统计 <!-- <input type="button" class="layui-btn" id="btnPrint" 
+onclick="onprint()" value="打印本页" /> --></h3>
+        <p>选择日期，再选择地点进行统计</p>
+        <p>时间是维修完成时间段</p>
+        <form action="" class="layui-form layui-form-pane" method="get" name="bxre">
             <?
-            if(isset($_GET['da1']))
-            {
-                $da1=$_GET['da1'];
-            }
-            else
-            {
-                $da1=$rqY.'-'.$rqmm.'-'.$rqd;
-            }
-            if(isset($_GET['da2']))
-            {
-                $da2=$_GET['da2'];
-            }
-            else
-            {
-                $da2=$rqY.'-'.$rqmm.'-'.$rqd;
-            }
+                if(isset($_GET['da1']))
+                {
+                    $da1=$_GET['da1'];
+                }
+                else
+                {
+                    $da1=$rqY.'-'.$rqmm.'-'.$rqd;
+                }
+                if(isset($_GET['da2']))
+                {
+                    $da2=$_GET['da2'];
+                }
+                else
+                {
+                    $da2=$rqY.'-'.$rqmm.'-'.$rqd;
+                }
             ?>
-          <input name="da1" type="date" value="<?=$da1?>"/>
-          至<input name="da2" type="date" value="<?=$da2?>"/>
-      <p>
-        
-        <button type="submit" name="alltj" class="btn btn-default">所有统计</button>
-        <p>
-        <?php
-            for($i=0;$i<count($arrayall);$i++)
-            {
-        ?>
-            <button type="submit" name="<?=$arrayalldm[$i]?>" class="btn btn-default"><?=$arrayall[$i]?></button>
-        <?php
-            }
-        ?>
-        </p>
-      </p>
-        
-    </form>
-    </p>
+            <div class="layui-form-item">
+                
+                <div class="layui-inline">
+                    <label class="layui-form-label">时间</label>
+                    <div class="layui-input-inline" style="width: 140px;">
+                        <input name="da1" class="layui-input" id="da1" type="text" value="<?=$da1?>"/>
+                    </div>
+                <div class="layui-form-mid">-</div>
+                <div class="layui-input-inline" style="width: 140px;">
+                    <input name="da2" class="layui-input" id="da2" type="text" value="<?=$da2?>"/>
+                </div>
+              </div>
+            </div> 
+
+            <div class="table-responsive">
+                <table width="100%" class="table" border="0" cellspacing="0" cellpadding="0">
+                    <tr>
+                        <td>
+                            <button type="submit" name="alltj" class="layui-btn layui-btn-sm layui-btn-normal">所有统计</button>
+                            <?php
+                                for($i=0;$i<count($arrayall);$i++)
+                                {
+                            ?>
+                                <button type="submit" name="<?=$arrayalldm[$i]?>" class="layui-btn layui-btn-sm"><?=$arrayall[$i]?></button>
+                            <?php
+                                }
+                            ?>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <script>
+            layui.use('laydate', function(){
+              var laydate = layui.laydate;
+              
+              //执行一个laydate实例
+              laydate.render({
+                elem: '#da1' //指定元素
+              });
+              laydate.render({
+                elem: '#da2' //指定元素
+              });
+            });
+            </script>
+        </form>
+    </blockquote>
+
     <p>
     
     <div class="container">
@@ -252,6 +281,6 @@ onclick="onprint()" value="打印本页" />-->
 </div>
     </p>
 </div>
-</center>
+
 </body>
 </html>
